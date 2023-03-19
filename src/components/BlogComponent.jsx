184e766a337552d4fb2da0420f6885a6e5a157bd
lupuioana <118757916+lupuioana@users.noreply.github.com>
2023-03-19 14:09:32 +0200

    first

---
+++ src/components/BlogComponent.jsx
@@ -0,0 +1,50 @@
+import { Card, CardBody, CardTitle, CardText, Button, Col } from "reactstrap";
+
+function BlogComponent({ blog }) {
+  const addToWishlist = () => {
+    const blogsString = localStorage.getItem("bloguri");
+
+    if (blogsString !== null) {
+      const blogs = JSON.parse(blogsString);
+
+      const existNr = blogs.find((blogElement) => {
+        return blogElement.id === blog.id;
+      });
+
+      if (existNr === undefined) {
+        blogs.push(blog);
+      }
+
+      localStorage.setItem("bloguri", JSON.stringify(blogs));
+    } else {
+      const newBlogs = [];
+      newBlogs.push(blog);
+      localStorage.setItem("bloguri", JSON.stringify(newBlogs));
+    }
+  };
+
+  return (
+    <Col className="mt-4">
+      <Card>
+        <img
+          alt="Sample"
+          src={"https://picsum.photos/300/200?random=" + blog.id}
+        />
+        <CardBody>
+          <CardTitle tag="h5">{blog.title}</CardTitle>
+          <CardText>{blog.body}</CardText>
+          <h4>{blog.userId}</h4>
+          <Button
+            onClick={() => {
+              addToWishlist();
+            }}
+          >
+            Add to favourite!
+          </Button>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+}
+
+export default BlogComponent;
---
+++ src/pages/Wishlist.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Button } from "reactstrap";
+
+function Wishlist() {
+  //Pasul 2 - se creaza o variabila de stare pentru datele noastre
+  const [wishlistBlogs, setWishlistBlogs] = useState([]);
+
+  //Pasul 1 - preluare date din localStorage
+  useEffect(() => {
+    const blogsString = localStorage.getItem("bloguri");
+    if (blogsString !== null) {
+      const blogs = JSON.parse(blogsString);
+      setWishlistBlogs(blogs);
+    }
+  }, []);
+
+  const onDelete = (blogId) => {
+    const filteredBlogs = wishlistBlogs.filter((blog) => blog.id !== blogId);
+    localStorage.setItem("bloguri", JSON.stringify(filteredBlogs));
+    setWishlistBlogs(filteredBlogs);
+  };
+
+  return (
+    <>
+      <h2>Wishlist page</h2>
+      <ul>
+        {wishlistBlogs.map((blog, index) => {
+          return (
+            <li key={"blog_" + index} className="mt-2">
+              {blog.title}
+              <Button
+                color="danger"
+                onClick={() => {
+                  onDelete(blog.id);
+                }}
+              >
+                Delete
+              </Button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default Wishlist;
